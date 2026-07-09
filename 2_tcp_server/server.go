31e2025d1523643131a9614e897e1a436a84d414
Maymona <syedamaymoona8@gmail.com>
2026-07-09 10:15:42 +0300

    Completed task 2 of establishing an agent-server connection

---
+++ 2_tcp_server/server.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	fmt.Println("Server started...")
+
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listener.Close()
+
+	fmt.Println("Waiting for connection...")
+
+	conn, err := listener.Accept()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Client connected!")
+
+	message, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading:", err)
+		return
+	}
+
+	fmt.Println("Message received:", message)
+}
