03e4c23d1be8e9434a5499356183e46b9b93a17e
Maymona <syedamaymoona8@gmail.com>
2026-07-09 10:15:02 +0300

    Worked on understanding tcp connections, using server and client

---
+++ 09-networking/03-system-agent/agent.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	
+	"learninggolang/1_system_monitor/cpu"
+	"learninggolang/1_system_monitor/disk"
+	"learninggolang/1_system_monitor/memory"
+)
+
+
+func main() {
+
+	cpuInfo, err := cpu.GetCPU()
+	if err != nil {
+		fmt.Println("Error retrieving CPU info: ", err)
+		return
+	}
+
+	memInfo, err := memory.GetMemory()
+	if err != nil {
+		fmt.Println("Error retrieving memory info: ", err)
+		return
+	}
+
+	diskInfo, err := disk.GetDisk("/")
+	if err != nil {
+		fmt.Println("Error retrieving disk info: ", err)
+		return
+	}
+
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Connected to server!")
+
+	
+	msg := fmt.Sprintf(
+		"CPU: %.1f%% | Memory: %.1f%% | Disk: %.1f%%\n",
+		cpuInfo.Usage,
+		memInfo.UsedPercent,
+		diskInfo.UsedPercent,
+	)
+
+	fmt.Fprint(conn, msg)
+}
